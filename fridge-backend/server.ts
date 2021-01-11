200a8f91f80f7af0d6b332877856af3d400fa1d3
Tobias Kullblikk <tobias@delegato.no>
2021-01-11 21:11:23 +0100

    test

--- fridge-backend/server.ts
+++ fridge-backend/server.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 
 app.get('/', (req, res, next) => {
-    res.send('Hello from API')
+    res.send('Hello from API v2')
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
